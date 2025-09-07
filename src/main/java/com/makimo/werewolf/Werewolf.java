f46e0cd10b134f4167b976434a0263a34eb8540c
Rithys <rithys.wisdom@gmail.com>
2025-09-07 23:26:41 +0900

    init

---
+++ src/main/java/com/makimo/werewolf/Werewolf.java
@@ -0,0 +1,12 @@
+package com.makimo.werewolf;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Werewolf.MOD_ID)
+public class Werewolf {
+    public static final String MOD_ID = "werewolf";
+
+    public Werewolf() {
+
+    }
+}
