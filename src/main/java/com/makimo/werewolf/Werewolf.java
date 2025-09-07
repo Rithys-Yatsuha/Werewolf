8965406be13930cce81997e0cf8020f83d6d3cfd
Rithys <rithys.wisdom@gmail.com>
2025-09-07 23:40:37 +0900

    make modEventBus

--- src/main/java/com/makimo/werewolf/Werewolf.java
+++ src/main/java/com/makimo/werewolf/Werewolf.java
@@ -1,12 +1,14 @@
 package com.makimo.werewolf;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Werewolf.MOD_ID)
 public class Werewolf {
     public static final String MOD_ID = "werewolf";
 
     public Werewolf() {
-
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     }
 }
